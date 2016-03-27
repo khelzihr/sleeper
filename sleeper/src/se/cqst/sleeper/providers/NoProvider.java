8dcbb79b16ff18fdfea3c52373b990e8b06a387c
khelzihr <nicklasrc@gmail.com>
2016-03-27 15:38:39 +0200

    Added valid constructor to NoProvider

--- sleeper/src/se/cqst/sleeper/providers/NoProvider.java
+++ sleeper/src/se/cqst/sleeper/providers/NoProvider.java
@@ -1,8 +1,10 @@
 package se.cqst.sleeper.providers;
 
+import java.util.HashMap;
+
 public class NoProvider implements Provider {
 	
-	public NoProvider()
+	public NoProvider(HashMap<String, String> arguments)
 	{
 		
 	}
