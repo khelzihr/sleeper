76810b81da1dbff64b1ea8f4b9d302a1711d6202
khelzihr <nicklasrc@gmail.com>
2016-03-27 21:06:46 +0200

    If provider is not set, NoProvider should terminate application

--- sleeper/src/se/cqst/sleeper/providers/NoProvider.java
+++ sleeper/src/se/cqst/sleeper/providers/NoProvider.java
@@ -6,7 +6,11 @@ public class NoProvider implements Provider {
 	
 	public NoProvider(HashMap<String, String> arguments)
 	{
-		
+		System.out.println("No provider has been set. Run this application without any arguments to display help.");
+		System.out.println("This application will now exit");
+		if(Boolean.valueOf(arguments.get("debug")))
+			System.out.println(arguments.toString());
+		System.exit(0);
 	}
 
 }
