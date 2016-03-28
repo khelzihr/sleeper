fa2ddd8320eeb5574c5a98ebbf033e963001b924
khelzihr <nicklasrc@gmail.com>
2016-03-28 16:18:18 +0200

    Moved getParser from GUMProvider to default implementation in Provider

--- sleeper/src/se/cqst/sleeper/providers/Provider.java
+++ sleeper/src/se/cqst/sleeper/providers/Provider.java
@@ -1,10 +1,55 @@
 package se.cqst.sleeper.providers;
 
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+
+import se.cqst.sleeper.parsers.Parser;
+
 public interface Provider {
 	
 	default boolean check()
 	{
 		return false;
+	}
+	
+	default Parser getParser(HashMap<String, String> arguments)
+	{
+		Parser parser = null;
+		
+		try
+		{
+			Object instance = null;
+			Class<?>	clazz = Class.forName(arguments.get("parser"));
+			Constructor<?> constructor = clazz.getConstructor(HashMap.class);
+			instance = constructor.newInstance(arguments);
+			if(instance instanceof se.cqst.sleeper.parsers.Parser)
+				parser = (Parser)instance;
+			else
+			{
+				System.out.println("Provider must be an implementation of se.cqst.sleeper.providers.Provider");
+				System.exit(0);
+			}
+		}
+		catch(ClassNotFoundException ex)
+		{
+			System.out.println("The parser \"" + arguments.get("parser") + 
+					"\" does not exist. Make sure you enter the full name of the class.");
+			ex.printStackTrace();
+			System.exit(0);
+		}
+		catch(NoSuchMethodException ex)
+		{
+			System.out.println("The parser \"" + arguments.get("parser") + 
+					"\" does not have a valid constructor (valid types are Parser(HashMap<String, String>))");
+			ex.printStackTrace();
+			System.exit(0);
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			System.exit(0);
+		}
+		return parser;
 	}
 
 }
