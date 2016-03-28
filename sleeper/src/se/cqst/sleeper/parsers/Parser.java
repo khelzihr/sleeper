837f826a1e46ef39bca6e85d66344648bb8cab9f
khelzihr <nicklasrc@gmail.com>
2016-03-28 14:23:53 +0200

    added String phrase to phraseExists()

--- sleeper/src/se/cqst/sleeper/parsers/Parser.java
+++ sleeper/src/se/cqst/sleeper/parsers/Parser.java
@@ -2,7 +2,7 @@ package se.cqst.sleeper.parsers;
 
 public interface Parser {
 	
-	default boolean phraseExists(String data)
+	default boolean phraseExists(String phrase, String data)
 	{
 		return false;
 	}
