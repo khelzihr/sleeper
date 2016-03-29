660f7d8d9eeeeb8e17671182cd16b2d468f81171
khelzihr <nicklasrc@gmail.com>
2016-03-29 13:58:40 +0200

    Added printHelp() and printUsage() javadoc

--- sleeper/src/se/cqst/sleeper/parsers/Parser.java
+++ sleeper/src/se/cqst/sleeper/parsers/Parser.java
@@ -36,11 +36,34 @@ public interface Parser {
 		System.out.println("[" + getDate() + "] " + text);
 	}
 	
+	/**
+	 * <p>Print detailed information about how you configure and use the Parser</p>
+	 * 
+	 * <p>Example: <i>Parser x reads incoming data backwards and looks for the keyphrase.
+	 * For key phrase <code>Foo</code>, ParserX.phraseExists() would return <code>true</code> if <code>ooF</code>
+	 * exists in the input data.</i></p>
+	 * 
+	 * <p><i>Valid arguments are: argumentA, argumentB, argumentC<br />
+	 * argumentA - do something<br />
+	 * argumentB - do something else<br />
+	 * argumentC - do something different</i></p>
+	 * 
+	 * @author Nicklas Rosvall Carlquist
+	 */
 	default void	printHelp()
 	{
 		print("This Parser has no help specified.");
 	}
 	
+	/**
+	 * <p>Print information about what the Parser will do. This information will be
+	 * displayed when you run the application with the specified Provider</p>
+	 * 
+	 * <p>Example: <i>Parser x will be used to find the specified key phrase by reading the
+	 * input data backwards. ArgumentA has been specified and parser x will do something.</i></p>
+	 * 
+	 * @author Nicklas Rosvall Carlquist
+	 */
 	default void	printUsage()
 	{
 		print("This Parser has no usage information defined.");
