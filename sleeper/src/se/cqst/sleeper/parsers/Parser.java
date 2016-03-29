389ae48265b808b7148f24c2f9f24781d4cb2d71
khelzihr <nicklasrc@gmail.com>
2016-03-29 13:17:49 +0200

    Added default methods getDate(), print(), printHelp(), printUsage()

--- sleeper/src/se/cqst/sleeper/parsers/Parser.java
+++ sleeper/src/se/cqst/sleeper/parsers/Parser.java
@@ -1,10 +1,49 @@
 package se.cqst.sleeper.parsers;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public interface Parser {
 	
 	default boolean phraseExists(String phrase, String data)
 	{
 		return false;
+	}
+	
+	/**
+	 * <p>Returns the current date</p>
+	 *
+	 * @return Current date in yyyy-MM-dd HH:mm format
+	 * 
+	 * @author Nicklas Rosvall Carlquist
+	 */
+	default String getDate()
+	{
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		return sdf.format(cal.getTime());
+	}
+	
+	/**
+	 * <p>Prints text to <code>System.out</code> with a prefix date in brackets.</p>
+	 *
+	 * @param text text to be printed
+	 * 
+	 * @author Nicklas Rosvall Carlquist
+	 */
+	default void print(String text)
+	{
+		System.out.println("[" + getDate() + "] " + text);
+	}
+	
+	default void	printHelp()
+	{
+		print("This Provider has no help specified.");
+	}
+	
+	default void	printUsage()
+	{
+		print("This provider has no usage information defined.");
 	}
 
 }
