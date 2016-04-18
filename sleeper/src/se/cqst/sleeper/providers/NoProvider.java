2ceea1131b7c6fa0c610b7c7c64928a585fab716
khelzihr <nicklasrc@gmail.com>
2016-04-18 13:29:39 +0200

    Added documentation to NoProvider

--- sleeper/src/se/cqst/sleeper/providers/NoProvider.java
+++ sleeper/src/se/cqst/sleeper/providers/NoProvider.java
@@ -2,6 +2,13 @@ package se.cqst.sleeper.providers;
 
 import java.util.HashMap;
 
+/**
+ * <p><strong>NoProvider</strong> is an implementation of the <code>Provider</code> interface that is used when no other
+ * <code>Provider</code> implementation has been specified.</p>
+ * 
+ * @author Nicklas Rosvall Carlquist
+ *
+ */
 public class NoProvider implements Provider {
 	
 	HashMap<String, String> arguments;
@@ -11,6 +18,12 @@ public class NoProvider implements Provider {
 		this.arguments = arguments;
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see se.cqst.sleeper.providers.Provider#check()
+	 * 
+	 * When check() is called, an error message will display and the application will exit.
+	 */
 	@Override
 	public boolean check()
 	{
